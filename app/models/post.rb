0b51e0f3fe7860a45ec50e0cb5b4a1657176f649
David <luptak.david@gmail.com>
2015-11-29 13:49:16 +0100

    Association HABTM.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ActiveRecord::Base
   validates_presence_of :author
   validates_presence_of :title
   validates_presence_of :body
+
+  has_and_belongs_to_many :tags
+
+  def tags_string
+
+  end
 end
