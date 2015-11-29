e437d3c668d24021cdd82e9990217c1767407a70
David <luptak.david@gmail.com>
2015-11-30 00:25:17 +0100

    Tag name as to_param default.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,11 @@ class Tag < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   has_and_belongs_to_many :posts
+
+  extend FriendlyId
+  friendly_id :name
+
+  # def to_param
+  #   self.name
+  # end
 end
